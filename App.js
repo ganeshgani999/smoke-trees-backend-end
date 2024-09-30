e5811f03c82b62bbdc43cde597f04678c114b918
Siva Ganesh <sivaganesh1399705@gmail.com>
2024-09-30 11:06:45 +0530

    final-commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoute');
 
 const app = express();
 
